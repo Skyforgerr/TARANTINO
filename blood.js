2aa6f6481868941c124ed0399af4a71cc6ee94cb
Skyforgerr <70031110+Skyforgerr@users.noreply.github.com>
2021-12-05 14:16:29 +0300

    all

---
+++ blood.js
@@ -0,0 +1,8 @@
+document.getElementById("reddd").addEventListener("mouseover", redddd);
+document.getElementById('reddd').addEventListener("mouseout", blackkk);
+function redddd(){
+	document.getElementById("reddd").style.background='url(https://t3.ftcdn.net/jpg/01/72/84/36/360_F_172843652_jJwPj7nM6VIotV8dcmN5iWCoxnQYPeuy.jpg)';
+}
+function blackkk(){
+	document.getElementById("reddd").style.background='white';
+}
